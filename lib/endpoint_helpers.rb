bfb8de240b4273f4a91bd61670384cc503e1a3b8
keithbooher <keith.booher@yahoo.com>
2020-04-14 16:48:00 -0500

    save

--- lib/endpoint_helpers.rb
+++ lib/endpoint_helpers.rb
@@ -15,11 +15,12 @@ module EndpointHelpers
       yield
     rescue Hubspot::RequestError => e
       if e.message.include? "Contact already exists" 
+        failure_message = {"customer": { "id": @id, "is_existing_customer": true }}
+
         puts "FAIL FAIL FAIL FAIL FAIL FAIL FAIL FAIL FAIL FAIL FAIL FAIL "
-        puts "customer": { "id": @id, "is_existing_customer": true }
+        puts failure_message
         puts "FAIL FAIL FAIL FAIL FAIL FAIL FAIL FAIL FAIL FAIL FAIL FAIL "
-        # result 500, "customer": { "id": @id, "is_existing_customer": true }
-        result 500, "cheese"
+        result 500, failure_message
       else
         result 500, e.message        
       end
