f3a3d54d74d7a450c348815ab36def75300d465c
keithbooher <keith.booher@yahoo.com>
2020-04-14 16:38:41 -0500

    trying still

--- lib/endpoint_helpers.rb
+++ lib/endpoint_helpers.rb
@@ -16,7 +16,7 @@ module EndpointHelpers
     rescue Hubspot::RequestError => e
       if e.message.include? "Contact already exists" 
         puts "FAIL FAIL FAIL FAIL FAIL FAIL FAIL FAIL FAIL FAIL FAIL FAIL "
-        puts "FAIL FAIL FAIL FAIL FAIL FAIL FAIL FAIL FAIL FAIL FAIL FAIL "
+        puts { "customer": { "id": @id, "is_existing_customer": true } }
         puts "FAIL FAIL FAIL FAIL FAIL FAIL FAIL FAIL FAIL FAIL FAIL FAIL "
         result 500, { "customer": { "id": @id, "is_existing_customer": true } }
       else
